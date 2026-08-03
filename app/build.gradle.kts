d47212122f2b89b1926a555c4c6c44cc0c539a0e
Raymond Gabriel <raymond.gabriel@gmail.com>
2026-08-03 15:54:09 +1200

    icon upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "rayzinnz.markdowntopdf"
         minSdk = 34
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
